1585a0ff767b8538c648bbced38a38a494f56807
sigitp <sigit.priyanggoro@gmail.com>
2024-09-05 15:01:30 -0500

    Update port-forward-amp-server.sh

--- port-forward-amp-server.sh
+++ port-forward-amp-server.sh
@@ -44,5 +44,4 @@ Events:            <none>
 Admin:~/environment $ 
 
 # port forward the endpoint IP and port via SSH to worker node
-ssh -i file.pem -L 8000:172.31.149.220:9090 ec2-user@ec2-184-72-87-109.compute-1.amazonaws.com
-ssh -i file.pem -L 8000:172.31.151.158:9090 ec2-user@ec2-34-229-218-164.compute-1.amazonaws.com
+ssh -i file.pem -L 8000:10.0.62.85:9090 ec2-user@54.152.145.162
